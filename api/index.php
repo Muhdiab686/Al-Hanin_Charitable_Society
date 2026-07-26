34b200a1191e6b32d4bc70c77520a92c60a558a6
Muhdiab686 <muhdiab686@gmail.com>
2026-07-27 00:05:33 +0300

    99

--- api/index.php
+++ api/index.php
@@ -1,9 +1,7 @@
 <?php
 
-// إجبار السيرفر على عرض جميع الأخطاء القاتلة
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-putenv('APP_DEBUG=true');
+// إخفاء تحذيرات الـ Deprecated لكي لا تدمر مخرجات JSON
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+ini_set('display_errors', '0');
 
 require __DIR__ . '/../public/index.php';
